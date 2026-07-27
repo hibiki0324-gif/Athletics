27fa05670de80060ee363dc9163ab3227a82fe0e
okajima <t.okajima.carecon@gmail.com>
2026-07-27 15:07:16 +0900

    共通コンポーネント作成（ヘッダー・フッター）

---
+++ frontend/src/components/layout/Footer.tsx
@@ -0,0 +1,11 @@
+
+function Footer(){
+    return(
+        <footer className="flex justify-center bg-gray-900 pt-[15px] pb-[15px]">
+            <p className="pr-[20px] text-white">©2026 アスレチックス</p>
+            <a className="text-white" href="#">GitHubリンク</a>
+        </footer>
+    )
+};
+
+export default Footer;
---
+++ frontend/src/components/layout/Header.tsx
@@ -0,0 +1,50 @@
+import logo from '../../assets/logo.png'
+
+
+function Header(){
+    return(
+        <div className='sticky top-0 z-50 bg-white'>
+            <div className='grid grid-cols-3 items-center'>
+                <div></div>
+                <div className="flex justify-center mt-[10px]">
+                    <img src={logo} alt='チームlogo' className='w-[20%] h-auto' />
+                </div>
+                <div className="flex justify-end pr-[50px]">
+                    <button>ログイン</button>
+                </div>
+            </div>
+            <div className='mt-[35px]'>
+                {/* ヘッダーは配列+mapで回した方がいい？ */}
+                <ul className='grid grid-cols-4 items-center'>
+                    <li className="font-medium">
+                        <a href="#" className="flex flex-col items-center text-xl">
+                            トップページ
+                            <span className="text-sm">TOP</span>
+                        </a>
+                    </li>
+                    <li className="font-medium">
+                        <a href="#" className="flex flex-col items-center text-xl">
+                            選手紹介
+                            <span className="text-sm">PLAYER</span>
+                        </a>
+                    </li>
+                    <li className="font-medium">
+                        <a href="#" className="flex flex-col items-center text-xl">
+                            試合結果
+                            <span className="text-sm">RESULT</span>
+                        </a>
+                    </li>
+                    <li className="font-medium">
+                        <a href="#" className="flex flex-col items-center text-xl">
+                            個人成績
+                            <span className="text-sm">STATS</span>
+                        </a>
+                    </li>
+                </ul>
+
+            </div>
+        </div>
+    )
+};
+
+export default Header;
